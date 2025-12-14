b85a79dfd1607828835048a9086de06973e5990d
handycnt <handycnt01@gmail.com>
2025-12-14 20:09:56 +0900

    Fix product detail composition data and images

---
+++ src/main/java/kr/co/matpam/admin/product/service/ProductImageVO.java
@@ -0,0 +1,66 @@
+package kr.co.matpam.admin.product.service;
+
+import java.io.Serializable;
+
+/**
+ * 판매상품 이미지 VO
+ */
+public class ProductImageVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long imageId;
+    private Long productNo;
+    private String imageType;
+    private String imagePath;
+    private String originalName;
+    private Integer sortOrder;
+
+    public Long getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(Long imageId) {
+        this.imageId = imageId;
+    }
+
+    public Long getProductNo() {
+        return productNo;
+    }
+
+    public void setProductNo(Long productNo) {
+        this.productNo = productNo;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public void setOriginalName(String originalName) {
+        this.originalName = originalName;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+}
